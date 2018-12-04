4cb850fd2ed943c29c81d5eb7e024d877f032862
Mitchell Larson <larsonma@msoe.edu>
2018-12-03 21:32:20 -0600

    Fixing Models

--- logisteps/models.py
+++ logisteps/models.py
@@ -3,15 +3,15 @@ from django.contrib.auth.models import User
 
 # Create your models here.
 class Location(models.Model):
-    latitude = models.models.FloatField()
-    longitude = models.models.FloatField()
+    latitude = models.FloatField()
+    longitude = models.FloatField()
 
     def __str__(self):
-        return self.name
+        return 'ID: {}, Latitude: {}, Longitude: {}'.format(self.id, self.latitude, self.longitude)
 
 class Shoe(models.Model):
-    size = models.models.DecimalField(max_digits=3, decimal_places=1)
-    is_left = models.models.models.BooleanField()
+    size = models.DecimalField(max_digits=3, decimal_places=1)
+    is_left = models.BooleanField()
 
     class Meta:
         verbose_name = "Logisteps Smart Shoe"
@@ -21,13 +21,12 @@ class Shoe(models.Model):
         return self.name
     
 class Step(models.Model):
-    date = models.models.DateField(auto_now=False, auto_now_add=False)
-    time = models.models.TimeField(auto_now=False, auto_now_add=False)
+    date = models.DateField(auto_now=False, auto_now_add=False)
+    time = models.TimeField(auto_now=False, auto_now_add=False)
     shoe = models.ForeignKey(Shoe, on_delete=models.CASCADE)
     location = models.OneToOneField(
         Location,
         on_delete=models.CASCADE,
-        primary_key=True,
     )
 
     class Meta:
@@ -38,16 +37,16 @@ class Step(models.Model):
         return self.name
 
 class LogistepsUser(models.Model):
-    user = models.models.OneToOneField(User, on_delete=models.CASCADE)
-    left_shoe = models.models.OneToOneField(
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    left_shoe = models.OneToOneField(
         Shoe,
-        on_delete=models.CASCADE,
-        primary_key=True,
+        related_name='user_left_shoe',
+        on_delete=models.CASCADE
     )
-    right_shoe = models.models.OneToOneField(
+    right_shoe = models.OneToOneField(
         Shoe,
-        on_delete=models.CASCADE,
-        primary_key=True,
+        related_name='user_right_shoe',
+        on_delete=models.CASCADE
     )
 
     class Meta:
