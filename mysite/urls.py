5b5fdab60a2e8f8f606a8861e0607b4fbe5e6e76
Mitchell Larson <larsonma@msoe.edu>
2018-12-13 19:52:25 -0600

    Removing polls demo app

--- mysite/urls.py
+++ mysite/urls.py
@@ -23,7 +23,6 @@ from django.urls import include, path
 urlpatterns = [
     path('accounts/', include('django.contrib.auth.urls')),
     path('api-auth/', include('rest_framework.urls')),
-    path('polls/', include('polls.urls')),
     path('logisteps/', include('logisteps.urls')),
     path('admin/', admin.site.urls),
     path('api/', include('logisteps_api.urls'))
