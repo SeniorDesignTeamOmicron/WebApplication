8e6538cce575225022eeebca43625173b42785f6
Mitchell Larson <larsonma@msoe.edu>
2019-02-10 18:43:23 -0600

    Fixed user creation process

--- logisteps/context_processors.py
+++ logisteps/context_processors.py
@@ -1,11 +1,15 @@
 from .models import LogistepsUser
 from django.contrib.auth.models import User
+from django.core.exceptions import ObjectDoesNotExist
 
 def logistepsUser(request):
     if request.user.is_anonymous:
         logisetpsUser = None
     else:
-        logisetpsUser = LogistepsUser.objects.get(user_id=request.user.id)
+        try:
+            logisetpsUser = LogistepsUser.objects.get(user_id=request.user.id)
+        except ObjectDoesNotExist:
+            logisetpsUser = None
     
     return {
         'logistepsUser': logisetpsUser
